ccd8796f364a6e92ff16cc7f10f80322b262a381
Pablo Gonzalez <pabloandres6@gmail.com>
2021-08-07 13:17:52 -0500

    addition statistics cards

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -83,8 +83,15 @@
     <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="#">Panel de administración</a>
-        <!-- Form -->
+
+        <!-- navegation -->
+        <ul class="nav text-uppercase">
+          <li class="nav-item">
+            <a class="nav-link active text-black" aria-current="page" href="#"><i class="fas fa-solar-panel"></i> Panel</a>
+          </li>
+        </ul>
+
+       <!-- Form -->
         <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
           <div class="form-group mb-0">
             <div class="input-group input-group-alternative">
@@ -114,8 +121,101 @@
         </div>
       </nav>
     <!-- Header -->
-    <div class="header bg-gradient-primary-dorado pb-8 pt-4 pt-md-6">
+        <!-- Header -->
+    <div class="header bg-gradient-primary-dorado pb-8 pt-5 pt-md-8">
+      <div class="container-fluid">
+        <div class="header-body">
+          <!-- Card stats -->
+          <div class="row">
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">BTC | (USD)</h5>
+                      <span class="h2 font-weight-bold mb-0">43.303,07</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                        <i class="fab fa-bitcoin"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-success mr-2"><i class="fas fa-dollar-sign"></i> 43.303</span>
+                    <span class="text-nowrap">Bitcoing Futurs</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Users</h5>
+                      <span class="h2 font-weight-bold mb-0">2,356</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
+                        <i class="fas fa-users"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-danger mr-2"><i class="fas fa-arrow-up"></i> 3.48%</span>
+                    <span class="text-nowrap">more users</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Comisión</h5>
+                      <span class="h2 font-weight-bold mb-0">5%</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                        <i class="fas fa-percent"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-warning mr-2"><i class="fas fa-dollar-sign"></i> 5.0%</span>
+                    <span class="text-nowrap">Comisión de nivel</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+              <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">RENDIMIENTO</h5>
+                      <span class="h2 font-weight-bold mb-0">20,0%</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-info text-white rounded-circle shadow">
+                        <i class="fas fa-coins"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20%</span>
+                    <span class="text-nowrap">En 30 dias</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
     <!-- Page content -->
     <div class="container-fluid mt--7">
       @yield('content')
